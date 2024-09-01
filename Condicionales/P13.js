4ca88d9145af2e43beb3c33848f890dcd81636c0
Juan17md <juan9182morales@gmail.com>
2024-08-31 20:04:55 -0400

    se completa ejercicio de condicional

--- Condicionales/P13.js
+++ Condicionales/P13.js
@@ -1,3 +1,31 @@
-monto = 150;
+/* En este ejercicio el script convertirá las notas numéricas 
+de un examen en paabras: 0 - 4 suspenso, 5-6 aprobado, 
+7-8 notable, 9-10 sobresaliente. */
 
-console.log(monto - ((monto * 5) / 100));
+let nota, msg;
+
+nota = parseInt(prompt('Ingresa la nota obtenida:'));
+
+switch(true){
+
+    case (nota < 5):
+        msg = 'Suspendido';
+        break;
+
+    case (nota >= 5  && nota < 7):
+        msg = 'Aprobado';
+        break;
+
+    case (nota >= 7 && nota < 9):
+        msg = 'Notable';
+        break;
+
+    case (nota >= 9 && nota <= 10):
+        msg = 'Sobresaliente';
+        break;
+
+    default:
+        msg = 'Valor Incorrecto'
+}
+
+alert('Tu nota fue: ' + msg)
